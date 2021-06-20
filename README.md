c470ced2e170c5292bef17b31169619f79e88b14
Dishit79 <Dishit79@gmail.com>
2021-06-20 01:29:57 -0400

    Rebased to cogs. Added Birdflop's timings analyzer and error handeling

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 DisoMcBot
 ======
 
-DisoMCBot is a Mineecraft stauts bot for SMP servers. It tells user when server is offline or online and how many players are playing.
+DisoMCBot is a Minecraft status bot for SMP servers. It provides the server's status and online players. You can add and remove cogs to customize the bots commands. The cogs are added automatically and default cog is `status.py`
 
 ## Installation
 1. Install all requirements using `pip install -r requirements.txt`
@@ -20,7 +20,7 @@ BOT_KEY="discord bot token"
 3. Run bot!
 
 ## Todo:
-1. Add error handling
-2. Add birdflop's timing analyser
-3. Rebase everything into cogs
+1. ~~Add error handling~~
+2. ~~Add birdflop's timing analyser~~
+3. ~~Rebase everything into cogs~~
 4. Make a docker
---
+++ cogs/status.py
@@ -0,0 +1,134 @@
+import discord
+from discord.ext import commands, tasks
+import asyncio
+from aiohttp import ClientSession
+from mcstatus import MinecraftServer
+import time
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+ip = os.getenv('IP')
+#imgur credintials
+client_id = os.getenv('CLIENT_ID')
+api_key = os.getenv('API_KEY')
+url = os.getenv('URL')
+
+
+class Status(commands.Cog):
+    def __init__(self,client):
+        self.client = client
+        self.server = MinecraftServer.lookup(ip)
+        self.img_cache = None
+        self.change_status.start()
+
+    async def fetchData(self):
+        try:
+            query = await self.server.async_status()
+            return {'status':'online','data':query}
+        except ConnectionRefusedError:
+            return {'status':'offline','data': None}
+        except asyncio.TimeoutError:
+            print ('e')
+            return await fetchData()
+        except Exception as e:
+            print (e.message, e.args)
+            print('ew')
+            server = MinecraftServer.lookup(ip)
+            return {'status':'nothing','data': None}
+
+    async def fetchImage(self,image):
+        if self.img_cache:
+            return self.img_cache
+        session = ClientSession()
+        submit = await session.post(
+            f'{url}/upload.json',
+            headers = {"Authorization": f"Client-ID {client_id}"},
+            data = {
+                'key': api_key,
+                'image': image,
+                'type': 'base64',
+                'name': '1.jpg',
+                'title': 'Picture no. 1'
+            }
+        )
+        data = await submit.json()
+        await session.close()
+        self.img_cache = data['data']['link']
+        return data['data']['link']
+
+    @commands.command(brief='Check server status')
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def status(self, ctx):
+        image = 'https://imgur.com/m84S3So.png'
+        data = await self.fetchData()
+        if data['status']=='online':
+            if data['data'].favicon:
+                image = await self.fetchImage(data['data'].favicon[22:] or None)
+            print(data['data'].description)
+            embed = discord.Embed(title="Minecraft Server Status", description=data['data'].description, color=0x78b159)
+            embed.set_footer(text=f'{ip} | Server Status v2.5 by Jhoan')
+            embed.set_thumbnail(url=image)
+            embed.add_field(name='Server Status: ', value=':green_circle: **| Online**', inline=False)
+            embed.add_field(name='Minecraft Version: ', value=data['data'].version.name, inline=False)
+            embed.add_field(name='Players Online: ', value=(f'{data["data"].players.online}/{data["data"].players.max}'), inline=True)
+            if 10 > data['data'].players.online > 0 :
+                embed.add_field(name='Players Playing : ', value=''.join(f"`{d.name}` " for d in data['data'].players.sample), inline=True)
+            await ctx.reply(embed=embed)
+        elif data['status']=='offline':
+            embed = discord.Embed(title="Minecraft Server Status", color=0xdd2e44)
+            embed.set_footer(text=f'{ip} | Server Status v2.5 by Jhoan')
+            embed.set_thumbnail(url=image)
+            embed.add_field(name='Server Status: ', value=':red_circle: **| Offline**', inline=False)
+            await ctx.reply(embed=embed)
+        else:
+            embed = discord.Embed(title=":x: **| Server not reachable**", color=0xE10600)
+            embed.set_footer(text=f'{ip} | Server Status v2.5 by Jhoan')
+            await ctx.send(embed=embed)
+
+    @status.error
+    async def logging_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.reply(f"Please try again in {str(error.retry_after)[:4]} seconds")
+        else:
+            print(error)
+            await ctx.reply("Something went wrong")
+
+    @commands.group(brief="View settings")
+    @commands.has_permissions(manage_messages=True)
+    async def settings(self, ctx):
+        await ctx.send(f"Img: {self.img_cache} \n Ip: {ip}")
+
+    @settings.command(brief="Reset server image")
+    async def reset(self, ctx):
+        self.img_cache= None
+        await ctx.send(':white_check_mark: Image cache reset')
+
+    @settings.command(brief="Change server ip")
+    async def ip_change(self, ctx, newip=ip):
+        ip = newip
+        await ctx.send(f':white_check_mark: Set ip to {newip}')
+
+    @settings.error
+    async def logging_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.reply("Sorry you can not use that command")
+        else:
+            print(error)
+            await ctx.reply("Something went wrong")
+
+    @tasks.loop(seconds=60)
+    async def change_status(self):
+        data = await self.fetchData()
+        if data['status']=='online':
+            activity = discord.Game(f"with {data['data'].players.online} friends | Online")
+            await self.client.change_presence(activity=activity)
+        elif data['status']=='offline':
+            activity = discord.Game(f"Offline | Last checked {time.strftime('%H:%M:%S', time.localtime())}")
+            await self.client.change_presence(status=discord.Status.dnd, activity=activity)
+        else:
+            activity = discord.Game(f"no server")
+            await self.client.change_presence(status=discord.Status.dnd, activity=activity)
+
+def setup(client):
+    client.add_cog(Status(client))
